f6a89e2748e2a09ea572f54b0f5690992e5409c5
Rahul Bhardwaj <rajputrahul445@gmail.com>
2025-03-28 13:36:11 +0530

    Banner design font size update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -76,8 +76,7 @@ const Home = () => {
                     <div className="row align-items-end">
                         <div className="col-md-5 col-12 pb-5">
                             <h1>Hire Virtual Employees from India — Pre-Vetted & Ready</h1>
-                            <h4>Work with India’s top 3% of remote talent in finance, tech, design, and support.<br/>
-                            Handpicked experts. No hiring hassle.</h4>
+                            <h4>Work with India’s top 3% of remote talent in finance, tech, design, and support. Handpicked experts. No hiring hassle.</h4>
                             <h5>Starts from <span className="greenText">$777/month</span> — Start your free trial today.</h5>
                             <Link to="/contact-us" className="colorBtn wideBtn">Start Your Free Trial Today</Link>
                         </div>
