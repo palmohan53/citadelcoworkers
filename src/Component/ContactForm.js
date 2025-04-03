5a1fe1d2b2db379773d74f77ad3be44a130e2eb2
Rahul Bhardwaj <rajputrahul445@gmail.com>
2025-04-03 19:26:25 +0530

    Speed issue fix

--- src/Component/ContactForm.js
+++ src/Component/ContactForm.js
@@ -82,61 +82,7 @@ const ContactForm = () => {
                                         <a href="https://wa.me/9891548011" style={{'color':'#5dcd84', 'borderColor': '#5dcd84'}}><FontAwesomeIcon icon={faWhatsapp} /> Whatsapp</a>
                                     </li>
                                 </ul>
-                                {/* <div className="formInr">
-                                    <div className="row">
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <input type="text" name="name" className="form-control" id="" placeholder="Name" /> 
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <input type="email" name="email" className="form-control" id="" placeholder="Email"/> 
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <input type="number" name="number" className="form-control" id="" placeholder="Phone Number"/> 
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <input type="text" name="Country" className="form-control" id="" placeholder="Website"/> 
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <select name="project_duration" className="form-control" id="">
-                                                    <option value="Project Duration">Project Duration</option>
-                                                    <option value="Less then one month">Less then one month</option>
-                                                    <option value="1 to 3 months">1 to 3 months</option>
-                                                    <option value="3 to 6 months">3 to 6 months</option>
-                                                    <option value="6 to 12 months">6 to 12 months</option>
-                                                    <option value="more then 1 year">more then 1 year</option>
-                                                    <option value="Not sure">Not sure</option>
-                                                </select> 
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-12">
-                                            <div className="form-group">
-                                                <input type="text" name="Country" className="form-control" id="" placeholder="Company Name"/> 
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12 col-12">
-                                            <div className="form-group">
-                                                <textarea name="" className="form-control" rows={5} placeholder="Share your description"></textarea>
-                                            </div>
-                                        </div>
-                                        <div className="d-flex mt-3 justify-content-center">
-                                            <button className="colorBtn wideBtn">Submit</button>
-                                        </div>
-                                    </div>
-                                </div> */}
+
                                 <div className="formInr">
                                     <form onSubmit={handleSubmit}>
                                         
@@ -266,10 +212,10 @@ const ContactForm = () => {
                                 </div>
                                 <Carousel infiniteLoop preventMovementUntilSwipeScrollTolerance={true} showIndicators={false} showStatus={false} showThumbs={false}>
                                     <div className="videoTestimonial">
-                                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/S8la1u6U1gc?si=vByJ9duuaLOLWP5k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                                        {/* <iframe width="100%" height="315" src="https://www.youtube.com/embed/S8la1u6U1gc?si=vByJ9duuaLOLWP5k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
                                     </div>
                                     <div className="videoTestimonial">
-                                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/o3GHmGSKA9I?si=i27sgTjBRKM_63wG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                                    {/* <iframe width="100%" height="315" src="https://www.youtube.com/embed/o3GHmGSKA9I?si=i27sgTjBRKM_63wG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
                                     </div>
                                 </Carousel>
                             </div>
