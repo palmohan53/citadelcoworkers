69e7aea9f1837e942a5c2bda6d7862a185039c91
Rahul Bhardwaj <rajputrahul445@gmail.com>
2025-04-06 00:36:25 +0530

    font weight issue fix

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -75,7 +75,7 @@ const Home = () => {
                 <div className="container">
                     <div className="row align-items-end">
                         <div className="col-md-5 col-12 bannerContent">
-                            <h1>Hire Virtual Employees from India</h1>
+                            <h1><b>Hire Virtual Employees from India</b></h1>
                             <h4>Work with India’s top 3% of remote talent in finance, tech, design, and support. Handpicked experts. No hiring hassle.</h4>
                             <h5>Starts from <span className="greenText">$777/month</span> — Start your free trial today.</h5>
                             <Link to="/contact-us" className="colorBtn wideBtn">Hire Remote Talent Now</Link>
