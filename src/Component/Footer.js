67ea0e2ea3998acf39cfd5cf178cdc6119d47183
Rahul Bhardwaj <rajputrahul445@gmail.com>
2025-04-06 11:02:35 +0530

    footer layout shift

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,43 +1,46 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faXTwitter, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+    const [footerHide, setFooterHide] = useState('d-none');
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY < 300) {
+                setFooterHide('d-none');
+            } else {
+                setFooterHide('');
+            }
+        });
+    }, [])
     return (
-        <>
-            <footer className="footer py-5">
+        <React.Fragment>
+            <footer className={`footer ${footerHide}`}>
                 <div className="container">
-                    <div className="row gy-4 justify-content-between">
-                        {/* Logo and Description */}
+                    <div className="row justify-content-between">
                         <div className="col-md-4 col-12">
                             <div className="footerLinks footerLogoBx">
-                                <div className="logo-wrapper" style={{ width: '180px', height: '60px', overflow: 'hidden' }}>
-                                    <Link to="/">
-                                        <img 
-                                            src="/images/logo-new-white.png" 
-                                            alt="Citadel Coworkers Logo" 
-                                            width="180" 
-                                            height="60" 
-                                            style={{ display: 'block', width: '100%', height: 'auto' }}
-                                            loading="eager"
-                                            decoding="async"
-                                        />
-                                    </Link>
+                                <div className="logo-wrapper">
+                                    <img 
+                                        src="/images/logo-new-white.png" 
+                                        alt="Citadel Coworkers Logo" 
+                                        width="180"
+                                        height="60"
+                                        className="logo-img"
+                                        loading="eager"
+                                        decoding="async"
+                                    />
                                 </div>
-                                <div className="content-wrapper mt-3">
-                                    <p>
-                                        Boost your business with our innovative outsourcing services. 
-                                        Elevate growth and productivity in a community designed for forward-thinkers.
-                                    </p>
+                                <div className="content-wrapper">
+                                    <p>Boost your business with our innovative outsourcing services. Elevate growth and productivity in a community designed for forward-thinkers.</p>
                                 </div>
                             </div>
                         </div>
 
-                        {/* Why Citadel */}
-                        <div className="col-md-2 col-6">
+                        <div className="col-md-2 col-12">
                             <div className="footerLinks mb-4">
-                                <h5 className="footerHeading">Why Citadel Coworkers</h5>
+                                <div className="footerHeading">Why Citadel Coworkers</div>
                                 <ul className="unorderList">
                                     <li><Link to="/about-citadel-advantage">Citadel Advantage</Link></li>
                                     <li><Link to="/how-does-citadel-works">How Does Citadel Works</Link></li>
@@ -47,10 +50,9 @@ const Footer = () => {
                             </div>
                         </div>
 
-                        {/* Policies */}
-                        <div className="col-md-2 col-6">
+                        <div className="col-md-2 col-12">
                             <div className="footerLinks mb-4">
-                                <h5 className="footerHeading">Policy</h5>
+                                <div className="footerHeading">Policy</div>
                                 <ul className="unorderList">
                                     <li><Link to="/refund-policy">Refund Policy</Link></li>
                                     <li><Link to="/privacy-policy">Privacy Policy</Link></li>
@@ -60,21 +62,23 @@ const Footer = () => {
                             </div>
                         </div>
 
-                        {/* Contact */}
-                        <div className="col-md-3 col-12">
+                        <div className="col-md-3 col-12 ftTouch">
                             <div className="footerLinks">
-                                <h5 className="footerHeading">Get In Touch</h5>
-                                <div className="mb-3">
-                                    <small className="footerSmlHeading">Location</small>
-                                    <p className="mb-1">HN – 59 Sector -62 Noida 201309</p>
+                                <div className="footerHeading">Get In Touch</div>
+                                <div className="d-block mb-4">
+                                    <div className="footerSmlHeading">Location</div>
+                                    <p>HN – 59 Sector -62 Noida 201309</p>
                                 </div>
-                                <div className="mb-2">
-                                    <small className="footerSmlHeading">Phone</small><br/>
-                                    <a href="tel:+16465830053">+1 (646) -583-0053</a>
-                                </div>
-                                <div>
-                                    <small className="footerSmlHeading">Mail Us</small><br/>
-                                    <a href="mailto:sales@citadelcoworkers.com">sales@citadelcoworkers.com</a>
+                                <div className="bottom">
+                                    <div className="footerSmlHeading">Contact</div>
+                                    <div className="con_content">
+                                        <div className="spt">Phone:</div>
+                                        <a href="tel:+16465830053">+1 (646) -583-0053</a>
+                                    </div>
+                                    <div className="con_content">
+                                        <div className="spt">Mail Us:</div>
+                                        <a href="mailto:sales@citadelcoworkers.com">sales@citadelcoworkers.com</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -82,45 +86,48 @@ const Footer = () => {
                 </div>
             </footer>
 
-            {/* Copyright */}
-            <div className="copyright py-3 border-top">
+            <div className={`copyright ${footerHide}`}>
                 <div className="container">
-                    <div className="row align-items-center gy-2">
-                        <div className="col-lg-6 text-center text-lg-start">
-                            <small>
+                    <div className="row d-flex justify-content-between align-items-center">
+                        <div className="col-lg-6 col-md-12">
+                            <div className="footer_copy_content">
                                 Copyright © 2009-2025. All Rights Reserved |
-                                Content belongs to Citadel Coworkers LLP & Virtual Assistant 24x7 LLC USA.
-                            </small>
+                                All content on this website belongs to Citadel Coworkers LLP & Virtual Assistant 24x7 LLC USA and is copyright protected.
+                            </div>
                         </div>
-                        <div className="col-lg-6">
-                            <ul className="listInline d-flex justify-content-center justify-content-lg-end gap-3 m-0 p-0">
-                                <li style={{ width: 32, height: 32 }}>
-                                    <a href="https://www.facebook.com/citadelcoworkers" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faFacebookF} size="lg" />
-                                    </a>
-                                </li>
-                                <li style={{ width: 32, height: 32 }}>
-                                    <a href="https://twitter.com/citadelcoworker" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faXTwitter} size="lg" />
-                                    </a>
-                                </li>
-                                <li style={{ width: 32, height: 32 }}>
-                                    <a href="https://www.linkedin.com/company/96461265/admin/feed/posts/" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
-                                    </a>
-                                </li>
-                                <li style={{ width: 32, height: 32 }}>
-                                    <a href="https://www.youtube.com/@CitadelCoworkers" target="_blank" rel="noreferrer">
-                                        <FontAwesomeIcon icon={faYoutube} size="lg" />
-                                    </a>
-                                </li>
-                            </ul>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="footer_copy_content_right">
+                                <div className="social_media_v_one">
+                                    <ul className="listInline justify-content-end">
+                                        <li>
+                                            <a href="https://www.facebook.com/citadelcoworkers" target="_blank" rel="noreferrer">
+                                                <FontAwesomeIcon icon={faFacebookF} size="lg" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://twitter.com/citadelcoworker" target="_blank" rel="noreferrer">
+                                                <FontAwesomeIcon icon={faXTwitter} size="lg" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.linkedin.com/company/96461265/admin/feed/posts/" target="_blank" rel="noreferrer">
+                                                <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.youtube.com/@CitadelCoworkers" target="_blank" rel="noreferrer">
+                                                <FontAwesomeIcon icon={faYoutube} size="lg" />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
-    );
-};
+        </React.Fragment>
+    )
+}
 
 export default Footer;
