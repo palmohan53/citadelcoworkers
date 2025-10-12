aaf6d8891ee0cacd25b359dd8fd679a350cb334f
Diksha sharma <diksha@example.com>
2025-10-12 16:18:49 +0530

    Updated code

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -14,6 +14,7 @@ import RecentBlog from "../Component/RecentBlog";
 
 const BlogDetails = () => {
     const {blog} = useParams();
+    
     const getBlogDetails = async () => {
         window.scrollTo(0, 0)
         const response = await axios.get(`${API_HOST}${API_ENDPOINTS.blogDetails}${blog}`)
